14cecb03ca6674c84c7e970640f4f766deb59d27
phucledien <phucledien@gmail.com>
2023-07-25 23:32:51 +0700

    fix: correct attribute name

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,12 +10,12 @@ export default function Navbar({ title }: { title: string }) {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth={2}
             // className="h-6 w-6 menu-icon"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M4 6h16M4 12h16M4 18h16"
             ></path>
           </svg>
