8ea654e9199b28a24d7a988dc3461f00b63fd92d
phucledien <phucledien@gmail.com>
2023-07-25 23:29:39 +0700

    fix: update activelink active logic

--- components/activelink.tsx
+++ components/activelink.tsx
@@ -12,13 +12,24 @@ export default function ActiveLink({
   href: string;
   shouldShowNewTab?: boolean;
 }) {
-  const { asPath } = useRouter();
+  const { asPath, pathname } = useRouter();
+
+  // TODO: Refactor this for dynamic routes
+  function isActive(asPath: string, pathname: string, href: string): boolean {
+    if (asPath === href) {
+      return true;
+    }
+    if (pathname === "/blog/[id]" && href === "/blog") {
+      return true;
+    }
+    return false;
+  }
   return (
     <Link
       className={utilStyles.activeLink}
       href={href}
       target={shouldShowNewTab ? "_blank" : null}
-      aria-current={asPath === href ? "page" : null}
+      aria-current={isActive(asPath, pathname, href) ? "page" : null}
     >
       {children}
     </Link>
